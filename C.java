9167920582624bb7666f383b554ab4b847a14d83
GitRepositoryStatiscicsDeveloper <85813892+GitRepositoryStatiscicsDeveloper@users.noreply.github.com>
2021-06-12 19:28:21 -0700

    First Commit

---
+++ C.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class C
+{
+    public static void main(String[] args) 
+    {
+        C c_obj=new C();
+        c_obj.M();
+
+    }     
+    public static void M() {
+        int r;
+        double pi = 3.14, area;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter the radius of circle:");
+        r = s.nextInt();
+        area = pi * r * r;
+        System.out.println("Area of circle:"+area);
+    }
+}
